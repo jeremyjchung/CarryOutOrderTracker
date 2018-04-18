acff030d32a0c0160ba8b9ddfdf2e30c3c58c868
Jeremy Chung <jeremyjchung@gmail.com>
2018-04-18 14:34:24 -0400

    file for testing notifications

---
+++ test_notifications.py
@@ -0,0 +1,7 @@
+from notifications import *
+
+if __name__ == '__main__':
+    n = Notifications()
+    ### use your own phone number only for testing ###
+    ### don't write a loop with this and use up all out $$$$ ###
+    #n.send_msg("+1**********", "hi")
