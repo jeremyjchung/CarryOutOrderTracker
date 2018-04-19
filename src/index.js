54ce2c54e9cc0f83e18eb2bf00c3c64a4b49546e
Jeremy Chung <jeremyjchung@gmail.com>
2018-04-19 17:43:13 -0400

    Set up webpack and react for front end development

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const title = 'Carry Out Order Tracker HI';
+
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('app')
+);
